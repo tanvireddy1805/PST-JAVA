1b896aea420a066568d3447f3d3cd5a32c1b5c02
tanvireddy1805 <tanvireddyp1805@gmail.com>
2026-07-26 14:45:44 +0530

    TASK2

---
+++ TASK2.java
@@ -0,0 +1,10 @@
+ public class  TASK2 {
+    public static void main(String[] args) {
+        
+        String[] fruits = {"Apple", "Banana", "Cherry", "Mango"};
+
+        int index = 2; 
+
+        System.out.println("Element at index " + index + " is: " + fruits[index]);
+    }
+}
